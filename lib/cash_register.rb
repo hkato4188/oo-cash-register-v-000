b787155a925a41f99495db8c3d4dd17d4851785e
Hiroki Kato <hkato@sas.upenn.edu>
2018-03-22 15:54:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
 
   def apply_discount
 binding.pry
-    self.total = total * (1 - (discount.to_f / 100))
+    self.total = (total * (1 - (discount.to_f / 100))).to_i
   end
 
 end
