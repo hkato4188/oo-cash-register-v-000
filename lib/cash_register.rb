434e13ce6f0d48a9444293ad501f0144aee464f9
Hiroki Kato <hkato@sas.upenn.edu>
2018-03-22 16:10:57 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,7 @@ class CashRegister
   end
 
   def apply_discount
+binding.pry
     if discount == 0
       return "There is no discount to apply."
     else
@@ -22,6 +23,7 @@ class CashRegister
     end
   end
 
+  def items()
 
 
 end
